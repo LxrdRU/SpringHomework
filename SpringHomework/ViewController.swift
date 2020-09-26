4746f46971f76542c15d6be817f5d2a24415b96e
Andrei MARKOV <a981203@mail.ru>
2020-09-27 00:55:48 +0300

    Initial Commit

---
+++ SpringHomework/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SpringHomework
+//
+//  Created by Andrey Markov on 2020-09-27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
